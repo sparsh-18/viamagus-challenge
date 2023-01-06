76f26a48feae3d7f7f3d6b523f02f2ca0ad3041a
sparsh-18 <sparsh.red@gmail.com>
2023-01-06 09:04:58 +0530

    configs added

--- server/src/app.module.ts
+++ server/src/app.module.ts
@@ -3,11 +3,17 @@ import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { TeamModule } from './modules/team/team.module';
 import { TypeOrmModule } from '@nestjs/typeorm';
-import { typeOrmConfig } from './configs/typeorm.config';
+import { typeOrmConfigAsync } from './configs/typeorm.config';
 import { TaskModule } from './modules/tasks/tasks.module';
+import { ConfigModule } from '@nestjs/config';
 
 @Module({
-  imports: [TeamModule, TaskModule, TypeOrmModule.forRoot(typeOrmConfig)],
+  imports: [
+    TeamModule,
+    TaskModule,
+    TypeOrmModule.forRootAsync(typeOrmConfigAsync),
+    ConfigModule.forRoot({ isGlobal: true }),
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
--- server/src/configs/typeorm.config.ts
+++ server/src/configs/typeorm.config.ts
@@ -1,15 +1,30 @@
-import { TypeOrmModuleOptions } from '@nestjs/typeorm';
+import { ConfigModule, ConfigService } from '@nestjs/config';
+import { TypeOrmModuleAsyncOptions, TypeOrmModuleOptions } from '@nestjs/typeorm';
 import { Task } from 'src/modules/tasks/tasks.entity';
 import { Team } from 'src/modules/team/entities/team.entity';
 import { TeamMember } from 'src/modules/team/entities/teamMembers.entity';
 
-export const typeOrmConfig: TypeOrmModuleOptions = {
-    type: 'mysql',
-    host: 'localhost',
-    port: 3306,
-    username: 'root',
-    password: '',
-    database: 'viamagus-challenge',
-    entities: [Team, TeamMember, Task],
-    synchronize: true,
-}
+// export default class TypeOrmConfig {
+//     static getOrmConfig(configService: ConfigService): TypeOrmModuleOptions {
+//         // console.log(configService.get('DB_HOST'));
+        
+//         return 
+//     }
+// }
+
+export const typeOrmConfigAsync: TypeOrmModuleAsyncOptions = {
+    imports: [ConfigModule],
+    inject: [ConfigService],
+    useFactory:
+        async (configService: ConfigService): Promise<TypeOrmModuleOptions> =>
+            ({
+                type: 'mysql',
+                host: configService.get('DB_HOST'),
+                port: configService.get('DB_PORT'),
+                username: configService.get('DB_USER'),
+                password: '',
+                database: configService.get('DB_NAME'),
+                entities: [Team, TeamMember, Task],
+                synchronize: true,
+            })
+};
